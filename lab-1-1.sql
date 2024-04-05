c798be6b5c1dfd34e2a9f48de81b740d2ddfbb08
Willy Chen <willy.chen@kellogg.northwestern.edu>
2024-04-05 19:42:53 +0000

    class exercise

--- lab-1-1.sql
+++ lab-1-1.sql
@@ -13,4 +13,6 @@
 -- | 2020 | Washington Nationals              |
 -- +------+-----------------------------------+
 
+SELECT year, name 
+FROM teams; 
 
--- lab-1-2.sql
+++ lab-1-2.sql
@@ -14,4 +14,6 @@
 -- | 2001 | Seattle Mariners                  | 116  |
 -- +------+-----------------------------------+------+
 
-
+SELECT year, name, wins
+FROM teams
+ORDER BY wins;
